c3b60544130bd0c4123c500d0080a80f1f99e8ee
Bruno B <contato4code@gmail.com>
2023-02-21 20:21:39 -0300

    comentarios tipo excepts

--- errors.py
+++ errors.py
@@ -1,7 +1,15 @@
 #!/usr/bin/venv python3
 
+import sys 
 
-names = open("names.txt").readlines()
-#LBYL - Look Before You Leap
-if len(names) >= 3:
-    print(names[1])
+#EAFP - EASY TO ASK FORGIVENESS THAN PERMISSION 
+try:
+    names = open("names.txt").readlines() #FileNotFoundError
+except FileNotFoundError: #bare except 
+    print("[error] file not found")
+
+try:
+    print(names[3])
+except:
+    print("Missing Name")
+    sys.exit()
